593a63dafb23abc410aaa498e1427143a797a4b4
Andrew Or <andrewor14@gmail.com>
2019-08-01 23:58:09 -0400

    Fix build and install script

--- build_and_install.sh
+++ build_and_install.sh
@@ -3,6 +3,9 @@
 source build_flags.sh
 
 "$PYTHON_BIN_PATH" setup.py bdist_wheel
+
+# Note: this MUST not be done in the horovod home directory
+cd dist
 "$PIP_COMMAND" uninstall -y horovod
-"$PIP_COMMAND" install --user --no-cache-dir dist/horovod*.whl
+"$PIP_COMMAND" install --user --no-cache-dir horovod*.whl
 
